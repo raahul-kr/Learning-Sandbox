b6d1f0a90774de65d9aad6fce733548e2dae2138
Rahul Kumar <147157203+raahul-kr@users.noreply.github.com>
2026-05-15 15:16:59 +0000

    datatypes

---
+++ Javascript/Javascript Beginner/01_Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// alert(3+3)  // we are using node js , not browser like same statement i can write in console and give the alert pop up
+
+// Types of values in js
+/*
+    number => 2 to power 53
+    bigint
+    string => ""
+    boolean => true/false
+    null => standalone value
+    undefined => 
+    symbol  => unique
+    object
+ */
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null); //object
+
+// EXPLORE ECMAScript link : https://tc39.es/ecma262/

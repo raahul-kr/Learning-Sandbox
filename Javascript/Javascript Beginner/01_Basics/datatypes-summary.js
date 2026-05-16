c8a5fe3e31ba332990b1d5045b3e156668433461
Rahul Kumar <147157203+raahul-kr@users.noreply.github.com>
2026-05-16 19:14:54 +0000

    primitive and non primitive

--- Javascript/Javascript Beginner/01_Basics/datatypes-summary.js
+++ Javascript/Javascript Beginner/01_Basics/datatypes-summary.js
@@ -33,3 +33,26 @@ const myFunction = function(){
 }
 
 console.log()
+
+
+//================================================
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "hiteshchoudharydotcom"
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);

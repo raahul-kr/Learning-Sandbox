4aaac9a57640e8824bacbb9acb51a40446905b5c
Rahul Kumar <147157203+raahul-kr@users.noreply.github.com>
2026-05-16 18:55:04 +0000

    datatypes-summary

---
+++ Javascript/Javascript Beginner/01_Basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive 
+
+// 7 Types : String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+// Reference (Non primitive)
+
+// Array , Objects, Functions
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id==anotherId);
+
+// array
+const heros = ["hero","villian"];
+
+// object 
+let myObj = {
+    name: "RAhul",
+    age : 22,
+}
+
+// function 
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log()

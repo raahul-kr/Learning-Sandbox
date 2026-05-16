f24c5ac4ff386b86ae865aad192db3b8ce3522ae
Rahul Kumar <147157203+raahul-kr@users.noreply.github.com>
2026-05-16 06:07:31 +0000

    operations and tricky one

--- Javascript/Javascript Beginner/01_Basics/03_conversionOperation.js
+++ Javascript/Javascript Beginner/01_Basics/03_conversionOperation.js
@@ -1,21 +1,52 @@
-// let score = 33
-let score = "33n" // gives otp -> NaN
+// // let score = 33
+// let score = "33n" // gives otp -> NaN
 
-console.log(typeof score);
-console.log(typeof(score)); // hm isse as method bhi likh skteh hai
+// console.log(typeof score);
+// console.log(typeof(score)); // hm isse as method bhi likh skteh hai
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-// BOOLEAN 
+// // BOOLEAN 
 
-let isLoggedIn = "Hitesh"
+// let isLoggedIn = "Hitesh"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-/* toh boolean k case me
-agar 1 -> true, 0-> false
-     "" -> false
-     "hitesh" ->true
-*/
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// /* toh boolean k case me
+// agar 1 -> true, 0-> false
+//      "" -> false
+//      "hitesh" ->true
+// */
+
+// *******************  Operations  ************************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "heelu"
+let str2 = "hitesh"
+let str3 = str1+str2
+console.log(str3);
+
+// tricky one 
+console.log("2"+2); //22
+console.log(2+"2");  //22
+console.log("1"+2+2); // 122
+console.log(1+2+"2"); //32
+
+// learn operator precedence
+
+console.log((3+4)*5%3)
+
+// search postfix and prefix mdn docs
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment

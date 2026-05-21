5c2222aa194facdd7319faac0d1423fddf498eec
Rahul Kumar <147157203+raahul-kr@users.noreply.github.com>
2026-05-21 17:40:22 +0000

    function part 2

--- Javascript/Javascript Beginner/03_basics/01_functions.js
+++ Javascript/Javascript Beginner/03_basics/01_functions.js
@@ -67,3 +67,42 @@ function loginUserMessage(username = "Ramu"){
 
 console.log(loginUserMessage("Rahul"))
 console.log(loginUserMessage())
+
+
+// function part 2
+
+function calculateCartPrice(...num1){  // this is  REST OPERATOT not a SPREAD OPERATOR
+    return num1
+}
+
+console.log(calculateCartPrice(200,300,400))
+
+
+//=======================================================
+
+
+const user = {
+    username : "Ritesh",
+    price : 199
+}
+
+function handleObject(anyobject){
+    console.log(`Usename is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+
+// handleObject(user); // or 
+handleObject({
+    username: "Ramu",
+    price: 3999
+})
+
+//------------------------------------------------------------------
+
+const myArray = [200, 400, 100, 600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myArray))
+console.log(returnSecondValue([200,300,400,500]))

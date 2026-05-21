63518d2ca482ff8f8ebe813b982f0ac232beec0f
Rahul Kumar <147157203+raahul-kr@users.noreply.github.com>
2026-05-21 17:21:51 +0000

    function part 1

---
+++ Javascript/Javascript Beginner/03_basics/01_functions.js
@@ -0,0 +1,69 @@
+// console.log("R");
+// console.log("A");
+// console.log("H");
+// console.log("U");
+// console.log("L");
+
+// function sayMyName(){
+//     console.log("R");
+//     console.log("A");
+//     console.log("H");
+//     console.log("U");
+//     console.log("L");
+    
+// }
+
+// sayMyName()
+
+function addTwoNumbers(number1, number2){ // has two parameters
+    console.log(number1+number2);
+}
+
+
+function addTwoNumbers(number1, number2){ // has two parameters
+    let result = number1 + number2
+    return result
+}
+
+
+const result = addTwoNumbers(3, 5); // has two arguments
+console.log("Result: ", result);
+
+
+
+//----------------------------------------------------------
+
+// function loginUserMessage(username){
+//     return `${username} just logged in`
+// }
+
+// console.log(loginUserMessage("Ramu"))
+
+
+//---------------------------------------------------------------
+
+// function loginUserMessage(username){
+//     if(!username){
+//         console.log("Please enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+
+// console.log(loginUserMessage("Rahul"))
+// // console.log(loginUserMessage())
+
+//-----------------------------------------------------------------
+
+//default value set kr skteh hai aur
+
+function loginUserMessage(username = "Ramu"){
+    if(!username){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("Rahul"))
+console.log(loginUserMessage())

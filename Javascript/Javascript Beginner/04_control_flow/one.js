baf60b07dedd040a4ca89775cd92386ecdb1845a
Rahul Kumar <147157203+raahul-kr@users.noreply.github.com>
2026-05-25 18:26:08 +0000

    basics fo control flow

---
+++ Javascript/Javascript Beginner/04_control_flow/one.js
@@ -0,0 +1,25 @@
+// if
+const isUserLoggedIn = true
+const temperature = 41
+
+// this is the way to 
+if(temperature === 41){
+    console.log("less than 50");
+} else {
+    console.log("temperatur is greater than 50");
+}
+
+// <,>,<=,>=, ==, !=, ===, !==
+
+const score = 200
+if(score >100){
+    let power = "fly"
+    console.log(`User power: ${power}`);
+
+}
+
+
+//=====================
+
+const balance = 1000
+if (balance>500) console.log("test"),console.log("test2");

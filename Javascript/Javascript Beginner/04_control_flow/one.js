ca2b697306f5c2c0344dbd8c7a601eeebc18192e
Rahul Kumar <147157203+raahul-kr@users.noreply.github.com>
2026-05-25 18:52:49 +0000

    basics of control flow one js

--- Javascript/Javascript Beginner/04_control_flow/one.js
+++ Javascript/Javascript Beginner/04_control_flow/one.js
@@ -1,25 +1,52 @@
-// if
-const isUserLoggedIn = true
-const temperature = 41
+// // if
+// const isUserLoggedIn = true
+// const temperature = 41
 
-// this is the way to 
-if(temperature === 41){
-    console.log("less than 50");
-} else {
-    console.log("temperatur is greater than 50");
+// // this is the way to 
+// if(temperature === 41){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperatur is greater than 50");
+// }
+
+// // <,>,<=,>=, ==, !=, ===, !==
+
+// const score = 200
+// if(score >100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+
+// }
+
+
+// //=====================
+
+// const balance = 1000
+// if (balance>500) console.log("test"),console.log("test2");
+
+
+// if(balance < 500){
+//     console.log("less than 500")
+// } else if(balance < 900){
+//     console.log("less than 750");
+// } else if(balance<900){
+//     console.log("less than 750")
+// } else{
+//     console.log("less than 1200")
+// }
+
+
+
+const UserLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+
+if(UserLoggedIn && debitCard && 2==2){
+    console.log("Allow to buy course")
 }
 
-// <,>,<=,>=, ==, !=, ===, !==
-
-const score = 200
-if(score >100){
-    let power = "fly"
-    console.log(`User power: ${power}`);
-
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("User logged In");
 }
-
-
-//=====================
-
-const balance = 1000
-if (balance>500) console.log("test"),console.log("test2");
